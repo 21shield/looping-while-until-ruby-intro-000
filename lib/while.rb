870a4c3249b35a2207a48a30ea3a235c1eaf1e9c
Netaly Ramirez <21netaly@gmail.com>
2020-03-24 13:40:58 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
 levitationForce = 0
 def using_while()
   while levitationForce <10
-  puts "Wingardium Leviosa"
-  levitationForce +=1
-end
+    puts "Wingardium Leviosa"
+    levitationForce +=1
+  end
 end
